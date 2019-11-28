d24b2ab39611fc5bf298abed78c4f848f95d3098
besigye <jamesbesigyek@gmail.com>
2019-11-28 16:17:05 +0300

    Initial VueJS project upload- not much done

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="Header">
+        <h1>
+            This is a vue course
+        </h1>
+    </div>  
+</template>
+<script>
+    export default {
+    name: "Header"
+    }
+</script>
+<style>
+    #Header{
+        color:red;
+    }
+</style>
